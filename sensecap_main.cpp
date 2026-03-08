40ea968d35c643d691ddc0b2aa92a0a0ba5a782d
Swarnarja <swarna20148@yahoo.com>
2026-03-07 21:14:15 -0800

    Integrate servo and ultrasonic sensor functionality
    Added servo control and ultrasonic distance measurement.

--- sensecap_main.cpp
+++ sensecap_main.cpp
@@ -1,11 +1,36 @@
-#include <Arduino.h>
-#include <lvgl.h>
+#include <Servo.h>
+
+Servo myServo;
+
+const int trigPin = 10;
+const int echoPin = 11;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);//initialize serial comms
+
+  myServo.attach(9);
+  delay(1000);
+  myServo.write(-180);   // start at 0 degrees
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  Serial.println("Hello world");
-  delay(1000);
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = duration * 0.034 / 2;
+
+  Serial.println(distance);
+
+  if (distance < 100) {
+      Serial.print("Restock");// restock indicator in terminal
+  }
 }
